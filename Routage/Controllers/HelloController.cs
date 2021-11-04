78477f603b6b37d328d4e1c8ac9af0692577754e
naim90 <naim.elmoudni@gmail.com>
2021-11-04 09:10:49 +0100

    Routage ASP quete

---
+++ Routage/Controllers/HelloController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routage.Controllers
+{
+
+    public class HelloController
+    {
+        [Route("/Say")]
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+        [Route("/Yell")]
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+
+        public String SayHelloHello()
+        {
+            return "Hello !";
+        }
+
+        public String YellHelloHello()
+        {
+            return "HELLO !";
+        }
+    }
+}
